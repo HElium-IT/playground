2a25da82c3c7daf4849223489cae5e65b880d926
HElium-IT <elio.palomba.dev@gmail.com>
2024-10-01 22:43:16 +0200

    refactor: changed from baklava to Vue-Flow

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <template>
   <div style="width: 100vw; height: 100vh">
-    <BaklavaEditor :view-model="baklava" />
+    <Header/>
+    <Grid />
   </div>
 </template>
 
 <script setup lang="ts">
-import { BaklavaEditor, useBaklava } from "@baklavajs/renderer-vue";
-import "@baklavajs/themes/dist/syrup-dark.css";
-import MyNode from "./nodes/MyNode";
+import { ref } from 'vue'  
+import Header from './components/Header.vue';
+import Grid from './components/Grid.vue';
 
-const baklava = useBaklava();
-baklava.editor.registerNodeType(MyNode);
+
+
 </script>
--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,7 +1,32 @@
 <template>
-
+    <div class="container">
+        <VueFlow v-model:nodes="nodes" v-model:edges="edges" ></VueFlow>
+    </div>
 </template>
 
 <script setup lang="ts">
+import { VueFlow } from '@vue-flow/core'
+import { ref } from 'vue';
 
+const nodes = ref([
+  { id: '1', type: 'input', label: 'Node 1', position: { x: 250, y: 5 } },
+  { id: '2', label: 'Node 2', position: { x: 100, y: 100 } },
+  { id: '3', label: 'Node 3', position: { x: 400, y: 100 } },
+  { id: '4', label: 'Node 4', position: { x: 400, y: 200 } },
+])
+  
+const edges = ref([
+  { id: 'e1-2', source: '1', target: '2', animated: true },
+  { id: 'e1-3', source: '1', target: '3' },
+])
 </script>
+
+<style>
+.container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100vw;
+    height: 100vh;
+}
+</style>
